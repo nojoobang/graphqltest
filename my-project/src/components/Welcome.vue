12a5e744d7afaa7a810b770b439b42dbcee49d7c
Jaewon Noh <jwno@knowre.com>
2017-07-18 17:25:37 +0900

    test

---
+++ my-project/src/components/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="welcome">
+		<div class="title">{{ mag }}</div>
+		<router-link to="/">Go to Hello</router-link>	
+	</div>
+</template>
+
+<script>
+export default {
+	name: '/welcome',
+	data () {
+		return {
+			msg: '웰컴이라는'
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.title {
+		font-size: 20px;
+		color: rgb(255, 200, 15);
+	}
+</style>
